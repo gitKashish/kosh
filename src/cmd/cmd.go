a6fdbde00819d26a2ad25d73a3a7438176da708f
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2025-10-26 01:23:31 +0530

    chore: update help cmd

--- src/cmd/cmd.go
+++ src/cmd/cmd.go
@@ -12,6 +12,8 @@ var Commands = map[string]Command{
 
 func Help(args ...string) {
 	fmt.Println("Usage:")
-	fmt.Println("  kosh init    - Initialize Kosh (vault)")
-	fmt.Println("  kosh help    - Show this help")
+	fmt.Println("  kosh init                - Initialize Kosh (vault)")
+	fmt.Println("  kosh help                - Show this help")
+	fmt.Println("  kosh add                 - Add a new credential to your vault to be retrieved later on")
+	fmt.Println("  kosh get <group> <user>  - Retrieve a stored secret by mentioning the group and the username for that credential")
 }
